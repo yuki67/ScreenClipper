7862983e59af048c5654e618d715da4a9af78b2d
yuki <123syokutaku@gmail.com>
2017-02-12 14:50:19 +0900

    screenshotを一つの関数に

--- ScreenClipper.py
+++ ScreenClipper.py
@@ -12,34 +12,12 @@ SCREEN_HEIGHT = 1440
 SCREEN_SCALING_FACTOR = 1.5
 
 
-class ScreenCamera(object):
-    """ スクリーンショットを取る(だけ) """
-    # PIL.ImageGrab.grab()がWindowsだとうまくいかないので自分で書き直した
-
-    # pylintでwin32guiの関数が見つからないというエラーが出るが、ちゃんと動く
-    def __init__(self):
-        self.window_dc = win32ui.CreateDCFromHandle(win32gui.GetWindowDC(win32gui.GetDesktopWindow()))
-        self.compatible_dc = self.window_dc.CreateCompatibleDC()
-        self.width = SCREEN_WIDTH
-        self.height = SCREEN_HEIGHT
-
-    def screenshot(self):
-        """ スクリーンショット撮ってそれを(Pillow.Imageで)返す """
-        bmp = win32ui.CreateBitmap()
-        bmp.CreateCompatibleBitmap(self.window_dc, self.width, self.height)
-        self.compatible_dc.SelectObject(bmp)
-        self.compatible_dc.BitBlt((0, 0), (self.width, self.height), self.window_dc, (0, 0), win32con.SRCCOPY)
-        img = Image.frombuffer('RGB', (self.width, self.height), bmp.GetBitmapBits(True), 'raw', 'BGRX', 0, 1)
-        return img
-
-
 class ScreenClipper(object):
     """ スクリーンショットを撮るための補助GUI """
     # tkinterが使う座標は全てスケールされたものであることに注意
 
     def __init__(self):
         # self.boxにはスケールされていない座標を使う
-        self.camera = ScreenCamera()
         self.root = tkinter.Tk()
         self.root.title("ScreenClipper")
         self.root.attributes("-alpha", 0.5)
@@ -63,12 +41,26 @@ class ScreenClipper(object):
         """ self.boxのスクリーンショットを取って保存する """
         dx = self.root.winfo_rootx() * 1.5
         dy = self.root.winfo_rooty() * 1.5 + 1
-        img = self.camera.screenshot().crop([dx,
-                                             dy,
-                                             dx + self.root.winfo_width() * SCREEN_SCALING_FACTOR,
-                                             dy + self.root.winfo_height() * SCREEN_SCALING_FACTOR])
+        img = self.screenshot().crop([dx,
+                                      dy,
+                                      dx + self.root.winfo_width() * SCREEN_SCALING_FACTOR,
+                                      dy + self.root.winfo_height() * SCREEN_SCALING_FACTOR])
         img.save("screenshot_" + datetime.now().strftime("%Y_%m_%d_%H_%M_%S") + ".jpg")
 
+    @staticmethod
+    def screenshot():
+        """ スクリーンショット撮ってそれを(Pillow.Imageで)返す """
+        window = win32gui.GetDesktopWindow()
+        window_dc = win32ui.CreateDCFromHandle(win32gui.GetWindowDC(window))
+        compatible_dc = window_dc.CreateCompatibleDC()
+        width = SCREEN_WIDTH
+        height = SCREEN_HEIGHT
+        bmp = win32ui.CreateBitmap()
+        bmp.CreateCompatibleBitmap(window_dc, width, height)
+        compatible_dc.SelectObject(bmp)
+        compatible_dc.BitBlt((0, 0), (width, height), window_dc, (0, 0), win32con.SRCCOPY)
+        img = Image.frombuffer('RGB', (width, height), bmp.GetBitmapBits(True), 'raw', 'BGRX', 0, 1)
+        return img
 
 if __name__ == "__main__":
     ScreenClipper()
