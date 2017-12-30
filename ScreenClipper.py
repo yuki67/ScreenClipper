f52e3d0d3207a00351884bf320b2071629e47da5
yuki@shiki <123syokutaku@gmail.com>
2017-12-30 23:04:24 +0900

    linuxに対応

--- ScreenClipper.py
+++ ScreenClipper.py
@@ -1,40 +1,51 @@
-import tkinter
+import platform
+import tkinter as tk
+import time
 from datetime import datetime
-import win32gui
-import win32ui
-import win32con
 from PIL import Image
 from MyGUI import MyTkBase
 
-# グローバル変数
-# どうしても無くせなかった
-SCREEN_WIDTH = 2160
-SCREEN_HEIGHT = 1440
-SCREEN_SCALING_FACTOR = 1.5
+if platform.system() == "Windows":
+    print("if saved image looks odd, "
+          "take a look at SCREEN_SCALING_FACTOR in ScreenClipper.py")
+    import win32gui
+    import win32ui
+    import win32con
+    # グローバル変数
+    # どうしても無くせなかった
+    SCREEN_SCALING_FACTOR = 1.5
+
+    def screenshot():
+        """ スクリーンショット撮ってそれを(Pillow.Imageで)返す """
+        window = win32gui.GetDesktopWindow()
+        window_dc = win32ui.CreateDCFromHandle(win32gui.GetWindowDC(window))
+        compatible_dc = window_dc.CreateCompatibleDC()
+        width = SCREEN_WIDTH
+        height = SCREEN_HEIGHT
+        bmp = win32ui.CreateBitmap()
+        bmp.CreateCompatibleBitmap(window_dc, width, height)
+        compatible_dc.SelectObject(bmp)
+        compatible_dc.BitBlt((0, 0), (width, height), window_dc, (0, 0),
+                             win32con.SRCCOPY)
+        img = Image.frombuffer('RGB', (width, height), bmp.GetBitmapBits(True),
+                               'raw', 'BGRX', 0, 1)
+        return img
+elif platform.system() == "Linux":
+    import pyscreenshot as ImageGrab
+    SCREEN_SCALING_FACTOR = 1.0
+    screenshot = ImageGrab.grab
+else:
+    print("error: unkonwn platform.")
+    exit(0)
 
 
-def screenshot():
-    """ スクリーンショット撮ってそれを(Pillow.Imageで)返す """
-    window = win32gui.GetDesktopWindow()
-    window_dc = win32ui.CreateDCFromHandle(win32gui.GetWindowDC(window))
-    compatible_dc = window_dc.CreateCompatibleDC()
-    width = SCREEN_WIDTH
-    height = SCREEN_HEIGHT
-    bmp = win32ui.CreateBitmap()
-    bmp.CreateCompatibleBitmap(window_dc, width, height)
-    compatible_dc.SelectObject(bmp)
-    compatible_dc.BitBlt((0, 0), (width, height), window_dc, (0, 0), win32con.SRCCOPY)
-    img = Image.frombuffer('RGB', (width, height), bmp.GetBitmapBits(True), 'raw', 'BGRX', 0, 1)
-    return img
-
-
-class ClipperButton(tkinter.Button):
+class ClipperButton(tk.Button):
     """ 押すとスクリーンショットが保存されるボタン """
 
     def __init__(self, master):
         super().__init__(master)
         self["text"] = "screenshot"
-        self["font"] = ("Arial", 20, "bold")
+        self["font"] = ("NanumBarunGothic", 20, "bold")
         self["bg"] = "blue"
         self["fg"] = "yellow"
         self.bind("<Button-1>", self.on_button_click)
@@ -42,41 +53,53 @@ class ClipperButton(tkinter.Button):
     def on_button_click(self, _):
         """ ウィンドウを透明にして画像を保存して透明度を元に戻す """
         self.master.attributes("-alpha", 0.0)
+        # アルファチャンネルの変更をフラッシュ
+        self.master.bell()
+        print("saving image...")
+        # ウィンドウを透明にする処理に時間がかかることがあるので適当に待つ
+        # 速度なんて誰も気にしない
+        time.sleep(1.0)
         self.clip_and_save()
+        # 処理が終わってからウィンドウを戻す
         self.master.attributes("-alpha", 0.5)
 
     def clip_and_save(self):
         """ self.boxのスクリーンショットを取って保存する """
-        dx = self.master.winfo_rootx() * 1.5
-        dy = self.master.winfo_rooty() * 1.5
-        img = screenshot().crop([dx,
-                                 dy,
-                                 dx + self.master.winfo_width() * SCREEN_SCALING_FACTOR,
-                                 dy + self.master.winfo_height() * SCREEN_SCALING_FACTOR])
-        img.save("screenshot_" + datetime.now().strftime("%Y_%m_%d_%H_%M_%S") + ".jpg")
+        dx = self.master.winfo_rootx() * SCREEN_SCALING_FACTOR
+        dy = self.master.winfo_rooty() * SCREEN_SCALING_FACTOR
+        img = screenshot().crop([
+            dx, dy, dx + self.master.winfo_width() * SCREEN_SCALING_FACTOR,
+            dy + self.master.winfo_height() * SCREEN_SCALING_FACTOR
+        ])
+        img.save("screenshot_" + datetime.now().strftime("%Y-%m-%d_%H:%M:%S") +
+                 ".jpg")
 
 
-class TextBox(tkinter.Label):
+class TextBox(tk.Label):
     """ 現在のウィンドウの大きさを表示するラベル """
 
     def __init__(self, master):
         super().__init__(master)
-        self["text"] = "size: %dx%d" % (master.winfo_width() * SCREEN_SCALING_FACTOR, master.winfo_height() * SCREEN_SCALING_FACTOR)
-        self["font"] = ("Arial", 20, "bold")
+        self["text"] = "size: %dx%d" % (
+            master.winfo_width() * SCREEN_SCALING_FACTOR,
+            master.winfo_height() * SCREEN_SCALING_FACTOR)
+        self["font"] = ("NanumBarunGothic", 20, "bold")
         self.bind('<Configure>', self.on_size_changed)
 
     def on_size_changed(self, _):
         """ テキストを変更 """
-        self["text"] = "size: %dx%d" % (self.master.winfo_width() * SCREEN_SCALING_FACTOR, self.master.winfo_height() * SCREEN_SCALING_FACTOR)
+        self["text"] = "size: %dx%d" % (
+            self.master.winfo_width() * SCREEN_SCALING_FACTOR,
+            self.master.winfo_height() * SCREEN_SCALING_FACTOR)
 
 
-class ResizeButton(tkinter.Button):
+class ResizeButton(tk.Button):
     """ 押されるとリサイズを実行するボタン """
 
     def __init__(self, master, entry):
         super().__init__(master)
         self["text"] = "resize"
-        self["font"] = ("Arial", 20, "bold")
+        self["font"] = ("NanumBarunGothic", 20, "bold")
         self.entry = entry
         self.bind("<Button-1>", self.on_button_click)
 
@@ -85,38 +108,43 @@ class ResizeButton(tkinter.Button):
         string = self.entry.get()
         try:
             w, h = string.split("x")
-            w, h = int(w) / SCREEN_SCALING_FACTOR, int(h) / SCREEN_SCALING_FACTOR
+            w, h = int(w) / SCREEN_SCALING_FACTOR, int(
+                h) / SCREEN_SCALING_FACTOR
         except ValueError:
             return
         self.master.geometry("%dx%d" % (int(w), int(h)))
 
 
-class ResizeEntry(tkinter.Entry):
+class ResizeEntry(tk.Entry):
     """ リサイズするときにサイズを入力する場所 """
 
     def __init__(self, master):
         super().__init__(master)
-        self["font"] = ("Arial", 20, "bold")
-        self.insert(tkinter.END, "%dx%d" % (master.winfo_width() * SCREEN_SCALING_FACTOR, master.winfo_height() * SCREEN_SCALING_FACTOR))
+        self["font"] = ("NanumBarunGothic", 20, "bold")
+        self.insert(tk.END,
+                    "%dx%d" % (master.winfo_width() * SCREEN_SCALING_FACTOR,
+                               master.winfo_height() * SCREEN_SCALING_FACTOR))
         self.bind('<Configure>', self.on_size_changed)
 
     def on_size_changed(self, _):
         """ テキストを変更 """
-        self.delete(0, tkinter.END)
-        self.insert(tkinter.END, "%dx%d" % (self.master.winfo_width() * SCREEN_SCALING_FACTOR, self.master.winfo_height() * SCREEN_SCALING_FACTOR))
+        self.delete(0, tk.END)
+        self.insert(tk.END, "%dx%d" %
+                    (self.master.winfo_width() * SCREEN_SCALING_FACTOR,
+                     self.master.winfo_height() * SCREEN_SCALING_FACTOR))
 
 
 class MyTk(MyTkBase):
-
     def __init__(self, width, height, name):
         super().__init__(width, height, name)
         self.attributes("-alpha", 0.5)
-        ClipperButton(self).pack()
-        TextBox(self).pack()
+        ClipperButton(self).pack(fill=tk.X)
+        TextBox(self).pack(fill=tk.X)
         entry = ResizeEntry(self)
-        entry.pack()
-        ResizeButton(self, entry).pack()
+        entry.pack(fill=tk.X)
+        ResizeButton(self, entry).pack(fill=tk.X)
         self.mainloop()
+
 
 if __name__ == "__main__":
     root = MyTk(500, 500, "Screen Clipper")
